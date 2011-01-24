ca900c464504b6dad1cb48ced8529a4fdbfd2147
angelod1981 <angelod1981@9c4cb45a-a489-e785-fa0a-971130d8ebe5>
2011-01-24 22:41:09 +0000

    [AndroidManifest.xml] * Added the new activity, as required.
    [/res/layout/writepm.xml]
    [/res/layout-land/writepm.xml]
    * The layout for the "Send PM" activity.
    
    [/res/values/strings.xml]
    * Added the value for the title of the "Send PM" activity.
    
    [list/ListPM.java]
    * Added a menu to allow the user to compose a new PM.
    * Formatted the source code.
    
    [/res/drawable/ic_menu_compose.png]
    * The graphics for the compose button.
    
    [/res/menu/listpmmenu.xml]
    * The menu layout for the "List PM" activity.
    
    [submits/SendPMActivity.java]
    * Allows the sending of PMs to users other than writing a reply.
    * Readied to ensure that the activity can be launched by other activities later, with the user ID and name of the recipient, which are then correctly filled in. This can be in relation to a submission by that user.
    * That feature has yet to be implemented however.

--- trunk/src/com/sofurry/list/ListPM.java
+++ trunk/src/com/sofurry/list/ListPM.java
@@ -1,12 +1,14 @@
 package com.sofurry.list;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import android.content.Context;
 import android.content.Intent;
+
 import android.util.Log;
+
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import android.widget.BaseAdapter;
 
 import com.sofurry.R;
@@ -15,65 +17,150 @@ import com.sofurry.itemviews.ViewPMActivity;
 import com.sofurry.listadapters.PrivateMessageAdapter;
 import com.sofurry.model.PrivateMessage;
 import com.sofurry.requests.AjaxRequest;
+import com.sofurry.submits.SendPMActivity;
 
-public class ListPM extends AbstractContentList<PrivateMessage> {
-
-	/* (non-Javadoc)
-	 * @see com.sofurry.AbstractContentList#onCreateOptionsMenu(android.view.Menu)
-	 * 
-	 * Suppresses the default options menu. Which would not make a terrible lot of sense in this context.
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		return false;
-	}
-
-	@Override
-	public AjaxRequest getFetchParameters(int page, int source) {
-		AjaxRequest req = new AjaxRequest();
-
-		req.addParameter("f", "pm");
-		req.addParameter("page", "" + page);
-		return req;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.sofurry.AbstractContentList#parseResponse(org.json.JSONObject)
-	 * 
-	 * Parses the Response for private messages
-	 */
-	@Override
-	public void parseResponse(JSONObject obj) {
-		try {
-			JSONArray items = new JSONArray(obj.getString("items"));
-			for (int i = 0; i < items.length(); i++) {
-				PrivateMessage m = new PrivateMessage();
-				m.populate(items.getJSONObject(i));
-				man.getResultList().add(m);
-				//man.getPageIDs().add("" + m.getId());
-			}
-		} catch (Exception e) {
-			man.onError(-1,e);
-		}
-	}
-
-	@Override
-	public void setSelectedIndex(int selectedIndex) {
-		PrivateMessage pm = getDataItem(selectedIndex);
-		//int pageID = Integer.parseInt(man.getPageIDs().get(selectedIndex));
-		Log.i("ListPM", "Viewing PM ID: " + pm.getId());
-		Intent i = new Intent(this, ViewPMActivity.class);
-		i.putExtra("PMID", pm.getId());
-		startActivity(i);
-	}
-
-	@Override
-	public BaseAdapter getAdapter(Context context) {
-		return new PrivateMessageAdapter(context, R.layout.listitemtwolineicon, man.getResultList());
-	}
-
-	public void resetViewSourceExtra(int newViewSource) {
-	}
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Class description
+ *
+ *
+ * @author SoFurry
+ */
+public class ListPM
+        extends AbstractContentList<PrivateMessage> {
+
+    /**
+     * Suppresses the default options menu, and replaces it with one that makes
+     * more sense for this context.
+     *
+     *
+     * @param menu
+     *
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.listpmmenu, menu);
+
+        return true;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param item
+     *
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.compose:
+                intent = new Intent(this, SendPMActivity.class);
+
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Parses the Response for private messages
+     *
+     *
+     * @param obj
+     */
+    @Override
+    public void parseResponse(JSONObject obj) {
+        try {
+            JSONArray items = new JSONArray(obj.getString("items"));
+
+            for (int i = 0; i < items.length(); i++) {
+                PrivateMessage m = new PrivateMessage();
+
+                m.populate(items.getJSONObject(i));
+                man.getResultList().add(m);
+
+                // man.getPageIDs().add("" + m.getId());
+            }
+        } catch (Exception e) {
+            man.onError(-1, e);
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param newViewSource
+     */
+    public void resetViewSourceExtra(int newViewSource) {}
+
+    //~--- get methods --------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @param context
+     *
+     * @return
+     */
+    @Override
+    public BaseAdapter getAdapter(Context context) {
+        return new PrivateMessageAdapter(context, R.layout.listitemtwolineicon, man.getResultList());
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param page
+     * @param source
+     *
+     * @return
+     */
+    @Override
+    public AjaxRequest getFetchParameters(int page, int source) {
+        AjaxRequest req = new AjaxRequest();
+
+        req.addParameter("f", "pm");
+        req.addParameter("page", "" + page);
+
+        return req;
+    }
+
+    //~--- set methods --------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @param selectedIndex
+     */
+    @Override
+    public void setSelectedIndex(int selectedIndex) {
+        PrivateMessage pm = getDataItem(selectedIndex);
+
+        // int pageID = Integer.parseInt(man.getPageIDs().get(selectedIndex));
+        Log.i("ListPM", "Viewing PM ID: " + pm.getId());
+
+        Intent intent = new Intent(this, ViewPMActivity.class);
+
+        intent.putExtra("PMID", pm.getId());
+        startActivity(intent);
+    }
 }
