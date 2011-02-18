0dd67d12cdd1450039035c6907c7fd53b7466954
angelod1981 <angelod1981@gmail.com>
2011-02-18 02:41:22 +0000

    [/res/layout/mainmenu.xml] * Since we're targeting Android 1.6, we might as well use the onClick feature that platform offers.
    [base/classes/ActivityWithRequests.java]
    * Added an extra sanity check to try and eliminate a NullPointerException error.
    * Checked the isOrientationLandscape() method to use the actual API to check the device orientation.
    
    [mainmenu/MainMenu.java]
    * Cleaned up the code, removing redundant code, and adding the new onClick handling.
    * Made it check the number of unread messages and show it on the PM button, but only if currently signed in, of course. ^^
    * The PM check runs whenever the activity resumes, but it has a built-in timer-check, ensuring that it only checks at most every five minutes.

--- src/com/sofurry/mainmenu/MainMenu.java
+++ src/com/sofurry/mainmenu/MainMenu.java
@@ -1,176 +1,335 @@
+/*
+ * @(#)MainMenu.java   2011-02-09
+ *
+ * Copyright (c) 2011 Tristan Bendixen
+ */
+
+
+
 package com.sofurry.mainmenu;
 
-import android.app.Activity;
+//~--- imports ----------------------------------------------------------------
+
 import android.app.AlertDialog;
+
 import android.content.Intent;
+
+import android.graphics.Typeface;
+
 import android.os.Bundle;
+
 import android.util.Log;
+
 import android.view.View;
+
 import android.widget.Button;
 
 import com.sofurry.AccountActivity;
 import com.sofurry.AppConstants;
 import com.sofurry.R;
+import com.sofurry.base.classes.ActivityWithRequests;
 import com.sofurry.chat.ChatActivity;
 import com.sofurry.gallery.GalleryArt;
 import com.sofurry.list.ListJournals;
 import com.sofurry.list.ListMusic;
 import com.sofurry.list.ListPM;
 import com.sofurry.list.ListStories;
+import com.sofurry.requests.AjaxRequest;
 import com.sofurry.util.Authentication;
 
+import org.json.JSONObject;
+
+import java.util.Date;
+
+
+//~--- classes ----------------------------------------------------------------
+
 /**
- * @author SoFurry
- *
  * Main menu activity
+ *
+ * @author SoFurry
  */
-public class MainMenu extends Activity {
+public class MainMenu
+        extends ActivityWithRequests {
+    private Button buttonChat_;
+    private Button buttonForums_;
+    private Button buttonLogbook_;
+    private Button buttonPMs_;
+    private int    messageCount_ = -1;
+    private long   lastCheck_    = -1;
 
-	Button buttonPMs;
-	Button buttonChat;
-	Button buttonLogbook;
-	Button buttonStories;
-	Button buttonArt;
-	Button buttonMusic;
-	Button buttonJournals;
-	Button buttonSettings;
-	Button buttonForums;
 
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Authentication.loadAuthenticationInformation(this);
-		setContentView(R.layout.mainmenu);
-		buttonPMs = (Button) findViewById(R.id.pms);
-		buttonChat = (Button) findViewById(R.id.chat);
-		buttonLogbook = (Button) findViewById(R.id.logbook);
-		buttonStories = (Button) findViewById(R.id.stories);
-		buttonArt = (Button) findViewById(R.id.art);
-		buttonMusic = (Button) findViewById(R.id.music);
-		buttonJournals = (Button) findViewById(R.id.journals);
-		buttonSettings = (Button) findViewById(R.id.settings);
-		buttonForums = (Button) findViewById(R.id.forums);
-		
-		buttonForums.setEnabled(false);
-		buttonLogbook.setEnabled(false);
-		//buttonMusic.setEnabled(false);
+    //~--- methods ------------------------------------------------------------
 
-		checkButtonDisabledState();
-		
-		buttonStories.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchStoriesList();
-			}
-		});
+    /**
+     * Method description
+     *
+     *
+     * @param v
+     */
+    public void buttonClick(View v) {
+        int    activityId = -1;
+        int    id         = v.getId();
+        Intent intent     = null;
 
-		buttonArt.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchArtGallery();
-			}
-		});
+        switch (id) {
+            case R.id.pms:
+                intent     = new Intent(this, ListPM.class);
+                activityId = AppConstants.ACTIVITY_PMLIST;
 
-		buttonMusic.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchMusicList();
-			}
-		});
+                break;
 
-		buttonJournals.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchJournalsList();
-			}
-		});
+            case R.id.chat:
+                intent = new Intent(this, ChatActivity.class);
 
-		buttonPMs.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchPMList();
-			}
-		});
+                break;
 
-		buttonChat.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchChat();
-			}
-		});
+            case R.id.logbook:
+                break;
 
-		buttonSettings.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View arg0) {
-				launchAccountActivity();
-			}
-		});
+            case R.id.stories:
+                intent     = new Intent(this, ListStories.class);
+                activityId = AppConstants.ACTIVITY_STORIESLIST;
 
-	}
+                break;
 
-	private void launchJournalsList() {
-		Intent intent = new Intent(this, ListJournals.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_JOURNALSLIST);
-	}
+            case R.id.art:
+                intent     = new Intent(this, GalleryArt.class);
+                activityId = AppConstants.ACTIVITY_GALLERYART;
 
-	private void launchArtGallery() {
-		Intent intent = new Intent(this, GalleryArt.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_GALLERYART);
-	}
+                break;
 
-	private void launchStoriesList() {
-		Intent intent = new Intent(this, ListStories.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_STORIESLIST);
-	}
+            case R.id.music:
+                intent     = new Intent(this, ListMusic.class);
+                activityId = AppConstants.ACTIVITY_MUSICLIST;
 
-	private void launchPMList() {
-		Intent intent = new Intent(this, ListPM.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_PMLIST);
-	}
+                break;
 
-	private void launchMusicList() {
-		Intent intent = new Intent(this, ListMusic.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_MUSICLIST);
-	}
+            case R.id.journals:
+                intent     = new Intent(this, ListJournals.class);
+                activityId = AppConstants.ACTIVITY_JOURNALSLIST;
 
-	private void launchAccountActivity() {
-		Intent intent = new Intent(this, AccountActivity.class);
-		startActivityForResult(intent, AppConstants.ACTIVITY_SETTINGS);
-	}
+                break;
 
-	private void launchChat() {
-		Intent intent = new Intent(this, ChatActivity.class);
-		startActivity(intent);
-	}
+            case R.id.settings:
+                intent     = new Intent(this, AccountActivity.class);
+                activityId = AppConstants.ACTIVITY_SETTINGS;
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		super.onActivityResult(requestCode, resultCode, intent);
-		Log.i("SF MainMenu", "onActivityResult");
-		checkButtonDisabledState();
+                break;
 
-		if (intent != null)
-		{
-			Bundle extras = intent.getExtras();
-			if (extras != null) {
-				// 	General error handling
-				String errorMessage = extras.getString("errorMessage");
-				if (errorMessage != null) {
-					new AlertDialog.Builder(MainMenu.this).setMessage(errorMessage).show();
-				}
-				
-				switch (requestCode) {
-				case AppConstants.ACTIVITY_STORIESLIST:
-					break;
-				case AppConstants.ACTIVITY_PMLIST:
-					break;
-				}
-			}
-		}
-	}
-	
-	private void checkButtonDisabledState() {
-		if (Authentication.getUsername() == null || Authentication.getUsername().trim().length() <= 0 ||
-				Authentication.getPassword() == null || Authentication.getPassword().trim().length() <= 0) {
-				buttonPMs.setEnabled(false);
-				buttonChat.setEnabled(false);
-		}
-		else
-		{
-			buttonPMs.setEnabled(true);
-			buttonChat.setEnabled(true);
-		}
-	}
+            case R.id.forums:
+                break;
+        }
 
+        // Check if we need to launch an activity
+        if (intent != null) {
+            // Will we be wanting the result?
+            if (activityId > -1) {
+                // Indeed we will
+                startActivityForResult(intent, activityId);
+            } else {
+                // We don't care about the result, no
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void checkButtonDisabledState() {
+        if ((Authentication.getUsername() == null)
+                || (Authentication.getUsername().trim().length() <= 0)
+                || (Authentication.getPassword() == null)
+                || (Authentication.getPassword().trim().length() <= 0)) {
+            buttonPMs_.setEnabled(false);
+            buttonChat_.setEnabled(false);
+        } else {
+            buttonPMs_.setEnabled(true);
+            buttonChat_.setEnabled(true);
+        }
+    }
+
+    private void checkPmCount() {
+    	// Limit how often it'll refresh itself, and only do so if information has been filled out
+        if ((buttonPMs_.isEnabled()) && (new Date().getTime() > lastCheck_ + 300000)) {
+            pbh.showProgressDialog("Fetching data...");
+            getCheckParameters().execute(requesthandler);
+
+            lastCheck_ = new Date().getTime();
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param intent
+     */
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        
+        // Check button state
+        checkButtonDisabledState();
+
+        // Check if we're coming from settings
+        if (requestCode == AppConstants.ACTIVITY_SETTINGS) {
+        	// Check for PMs
+        	checkPmCount();
+        }
+        
+        if (intent != null) {
+            Bundle extras = intent.getExtras();
+
+            if (extras != null) {
+                // General error handling
+                String errorMessage = extras.getString("errorMessage");
+
+                if (errorMessage != null) {
+                    new AlertDialog.Builder(MainMenu.this).setMessage(
+                        errorMessage).show();
+                }
+
+                switch (requestCode) {
+                    case AppConstants.ACTIVITY_STORIESLIST:
+                        break;
+
+                    case AppConstants.ACTIVITY_PMLIST:
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param savedInstanceState
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Retrieve authentication info
+        Authentication.loadAuthenticationInformation(this);
+
+        // Set the content view
+        setContentView(R.layout.mainmenu);
+
+        // Find the buttons we'll be using
+        buttonPMs_     = (Button) findViewById(R.id.pms);
+        buttonChat_    = (Button) findViewById(R.id.chat);
+        buttonLogbook_ = (Button) findViewById(R.id.logbook);
+        buttonForums_  = (Button) findViewById(R.id.forums);
+
+        // Disable forums and logbook buttons for now
+        buttonForums_.setEnabled(false);
+        buttonLogbook_.setEnabled(false);
+
+        // Disable buttons as need be
+        checkButtonDisabledState();
+
+        // Retrieve information from the instance state object, if need be
+        if (savedInstanceState != null) {
+            messageCount_ = (Integer) retrieveObject("messageCount");
+
+            updateButtons();
+        } else {
+            // Fetch the information from the server instead
+            Log.i("[AngelOD]", "onCreate check");
+            checkPmCount();
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param id
+     * @param obj
+     *
+     * @throws Exception
+     */
+    @Override
+    public void onData(int id, JSONObject obj) throws Exception {
+        if (id == AppConstants.REQUEST_ID_FETCHDATA) {
+            // Hide progress bar
+            pbh.hideProgressDialog();
+
+            // Fetch the number
+            messageCount_ = obj.getInt("unreadpmcount");
+
+            // Update display
+            updateButtons();
+        } else {
+            super.onData(id, obj);
+        }
+    }
+
+    /**
+     * Method description
+     *
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Check if we need to check for messages
+        if (new Date().getTime() > lastCheck_ + 300000) {
+            Log.i("[AngelOD]", "onResume check");
+            checkPmCount();
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        storeObject("messageCount", (Integer) messageCount_);
+    }
+
+    /**
+     * Method description
+     *
+     */
+    protected void updateButtons() {
+        // Alter the PM button based on the number of messages
+        if (messageCount_ > 0) {
+            buttonPMs_.setTypeface(Typeface.create((String) null,
+                                                   Typeface.BOLD));
+        } else {
+            buttonPMs_.setTypeface(Typeface.create((String) null,
+                                                   Typeface.NORMAL));
+        }
+
+        // Including the text
+        buttonPMs_.setText("PMs (" + messageCount_ + ")");
+    }
+
+    //~--- get methods --------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    protected AjaxRequest getCheckParameters() {
+        AjaxRequest req = new AjaxRequest();
+
+        // Set request parameters
+        req.addParameter("f", "unreadpmcount");
+
+        // Set request ID
+        req.setRequestID(AppConstants.REQUEST_ID_FETCHDATA);
+
+        // Return result
+        return req;
+    }
 }
